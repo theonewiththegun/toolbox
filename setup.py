b8caba5099a592d6c06fb77d172d9a5bd44ba1b8
Igor Popkov <popkov97.97.97@gmail.com>
2022-02-12 20:21:56 +0300

    # Added a setup file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="toolbox",
+    version="0.0.1",
+    author="Igor Popkov",
+    author_email="popkov97.97.97@gmail.com",
+    description="A set of tools for data science.",
+    long_description='# TBD',
+    long_description_content_type="text/markdown",
+    url="https://github.com/theonewiththegun/toolbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
