be44a8378c87673adf5a36f74d0b047df39eb438
Igor Popkov <popkov97.97.97@gmail.com>
2022-02-12 20:32:48 +0300

    # Used readme as a long package description.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import setuptools
 
+with open("README.md", "r") as handle:
+    long_description = handle.read()
+
 setuptools.setup(
     name="toolbox",
     version="0.0.1",
     author="Igor Popkov",
     author_email="popkov97.97.97@gmail.com",
     description="A set of tools for data science.",
-    long_description='# TBD',
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/theonewiththegun/toolbox",
     packages=setuptools.find_packages(),
